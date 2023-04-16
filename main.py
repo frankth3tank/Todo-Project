9cc8d22d428086cf58c4b601a8a7797b3cbc93b7
Francisquet Vargas <fvs00725@gmail.com>
2023-04-16 14:50:27 -0500

    added get request to get all the todos from the database

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import models
-from fastapi import FastAPI
-from database import engine
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+        
+
+
 @app.get("/")
-async def create_database() -> str:
-    return {"Database": "Created"}
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
