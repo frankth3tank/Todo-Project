73ef7394ed110528a80816701dbbf067b137d96e
Francisquet Vargas <fvs00725@gmail.com>
2023-04-16 15:15:17 -0500

    added get request to get Todo by ID

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import models
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
@@ -14,8 +14,16 @@ def get_db():
     finally:
         db.close()
         
-
-
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+@app.get("/todo/{id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise http_exception()
+
+def http_exception():
+    raise HTTPException(status_code = 404, detail = "Todo not found")
