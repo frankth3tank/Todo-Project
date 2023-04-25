e5d1bebd535707bc799e0b46387bd65e280b3835
Francisquet Vargas <fvs00725@gmail.com>
2023-04-25 13:46:55 -0500

    added authentication for get requests, so it can fetch specific data for logged user

--- routers/todos.py
+++ routers/todos.py
@@ -28,11 +28,19 @@ class Todo(BaseModel):
         
 @router.get("/", status_code = status.HTTP_200_OK)
 async def read_all(user: user_dependency ,db: db_dependency):
+    
+    if user is None:
+        raise HTTPException(status_code = 401, detail = "Authentication failed")
+    
     return db.query(Todos).filter(Todos.owner_id == user.get("id")).all()
 
 @router.get("/todo/{id}")
-async def read_todo(todo_id: int, db: db_dependency):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def read_todo(user: user_dependency, todo_id: int, db: db_dependency):
+    
+    if user is None:
+        raise HTTPException(status_code = 401, detail = "Authentication failed")
+    
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get("id")).first()
     
     if todo_model is not None:
         return todo_model
